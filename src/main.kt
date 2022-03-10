2467425b2cc94cd0bdf98ff227b41e09f1989e8c
wangare <marywangareee@gmail.com>
2022-03-10 08:51:13 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,29 @@
+fun main() {
+
+    var number=(12%45)
+    println(number)
+
+    var s=statement("Mary", 24)
+    println(s)
+
+    var state=state("Mellisa")
+    println(state.length)
+
+}
+fun word(text:String){
+    var names="CodeHive"
+    var word=("names[6], name[7],name[8]")
+    println(word)
+}
+fun modulous(a:Int,b:Int):Int{
+    var number=a%b
+    return number
+}
+fun statement(p:String,z:Int):String{
+    var s=("Hi, my name is $p and i am $z years old")
+    return s
+}
+fun state(y:String):String{
+    var states= state("Mellisa")
+    return states
+}
